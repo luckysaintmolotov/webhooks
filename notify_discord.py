678185ad9e81339c9ada25c72dbb32653b5d8124
luckysaintmolotov <209353814+luckysaintmolotov@users.noreply.github.com>
2026-05-16 04:06:09 +0200

    refactor to include event id ,device id and session type

--- notify_discord.py
+++ notify_discord.py
@@ -2,8 +2,17 @@ import requests,os
 from dotenv import load_dotenv
 load_dotenv()
 DISCORD_WEBHOOK_URL = os.getenv("DISCORD_WEBHOOK_URL")
-def send_discord_message(direct_url, status, timestamp):
-    content = f"NEW WEBHOOK EVENT!!\nSTATUS: **{status.upper()}**\nTIME: {timestamp}\nURL{direct_url}"
+def send_discord_message(direct_url, status, timestamp, event_id,session_type,device_id):
+    content = f"""
+-------------------------------------------------------------------------------------------    
+    NEW WEBHOOK EVENT!!
+    \n EVENT_ID: **{event_id}**
+    \n DEVICE ID: **{device_id}**
+    \n SESSION TYPE: **{session_type.upper()}**
+    \nSTATUS: **{status.upper()}**
+    \nTIME: {timestamp}
+    \nURL--{direct_url}
+--------------------------------------------------------------------------------------------"""
     try: 
         response = requests.post(DISCORD_WEBHOOK_URL, json={"content":content})
         if response.status_code == 204:
@@ -16,4 +25,4 @@ def send_discord_message(direct_url, status, timestamp):
         
         
 if __name__ == "__main__":
-    send_discord_message("direct_url","status","timestamp")
+    send_discord_message("direct_url","status","timestamp","event_id","session_type","device_id")
