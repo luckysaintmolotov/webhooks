911658a2d3a02342bdb04e85cf1e4ca851ddd699
luckysaintmolotov <209353814+luckysaintmolotov@users.noreply.github.com>
2026-05-16 02:59:09 +0200

    add notify_discord.py to send notifications via Discord webhook

---
+++ notify_discord.py
@@ -0,0 +1,19 @@
+import requests,os
+from dotenv import load_dotenv
+load_dotenv()
+DISCORD_WEBHOOK_URL = os.getenv("DISCORD_WEBHOOK_URL")
+def send_discord_message(direct_url, status, timestamp):
+    content = f"NEW WEBHOOK EVENT!!\nSTATUS: **{status.upper()}**\nTIME: {timestamp}\nURL{direct_url}"
+    try: 
+        response = requests.post(DISCORD_WEBHOOK_URL, json={"content":content})
+        if response.status_code == 204:
+            print("Discord Notification Sent")
+        else:
+            print(f"Discord error: {response.status_code}  {response.text}")
+            
+    except Exception as e:
+        print(f"failed to send Discord message: {e}")
+        
+        
+if __name__ == "__main__":
+    send_discord_message("direct_url","status","timestamp")
