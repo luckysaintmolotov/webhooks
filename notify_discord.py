8a78dc83d7068cdfa76c0d836c08b87b6a2a00ba
luckysaintmolotov <209353814+luckysaintmolotov@users.noreply.github.com>
2026-05-19 02:19:29 +0200

    refactor send_discord_message to differentiate between test/verify and regular webhook events

--- notify_discord.py
+++ notify_discord.py
@@ -3,26 +3,49 @@ from dotenv import load_dotenv
 load_dotenv()
 DISCORD_WEBHOOK_URL = os.getenv("DISCORD_WEBHOOK_URL")
 def send_discord_message(direct_url, status, timestamp, event_id,session_type,device_id):
-    content = f"""
+    if int(event_id) == 1 :
+        content = f"""
 -------------------------------------------------------------------------------------------    
-    NEW WEBHOOK EVENT!!
-    \n EVENT_ID: **{event_id}**
-    \n DEVICE ID: **{device_id}**
-    \n SESSION TYPE: **{session_type.upper()}**
-    \nSTATUS: **{status.upper()}**
-    \nTIME: {timestamp}
-    \nURL--{direct_url}
+        NEW TEST/VERIFY WEBHOOK EVENT!!
+        \n EVENT_ID: **TEST/VERIFY**
+        \n DEVICE ID: **{device_id}**
+        \n SESSION TYPE: **{session_type.upper()}**
+        \nSTATUS: **{status.upper()}**
+        \nTIME: {timestamp}
+        \nURL--{direct_url}
 --------------------------------------------------------------------------------------------"""
-    try: 
-        response = requests.post(DISCORD_WEBHOOK_URL, json={"content":content})
-        if response.status_code == 204:
-            print("Discord Notification Sent")
-        else:
-            print(f"Discord error: {response.status_code}  {response.text}")
+        try: 
+            response = requests.post(DISCORD_WEBHOOK_URL, json={"content":content})
+            if response.status_code == 204:
+                print("Discord Notification Sent")
+            else:
+                print(f"Discord error: {response.status_code}  {response.text}")
+                
+        except Exception as e:
+            print(f"failed to send Discord message: {e}")
+        
+        
+    else:
+        content = f"""
+-------------------------------------------------------------------------------------------    
+        NEW WEBHOOK EVENT!!
+        \n EVENT_ID: **{event_id}**
+        \n DEVICE ID: **{device_id}**
+        \n SESSION TYPE: **{session_type.upper()}**
+        \nSTATUS: **{status.upper()}**
+        \nTIME: {timestamp}
+        \nURL--{direct_url}
+--------------------------------------------------------------------------------------------"""
+        try: 
+            response = requests.post(DISCORD_WEBHOOK_URL, json={"content":content})
+            if response.status_code == 204:
+                print("Discord Notification Sent")
+            else:
+                print(f"Discord error: {response.status_code}  {response.text}")
+                
+        except Exception as e:
+            print(f"failed to send Discord message: {e}")
             
-    except Exception as e:
-        print(f"failed to send Discord message: {e}")
-        
-        
-if __name__ == "__main__":
-    send_discord_message("direct_url","status","timestamp","event_id","session_type","device_id")
+
+    if __name__ == "__main__":
+        send_discord_message("direct_url","status","timestamp","event_id","session_type","device_id")
